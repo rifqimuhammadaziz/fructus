ce2d20f79fcf1cb1844623351567fc421b4ca472
rifqimuhammadaziz <rifqimuhammadaziz@gmail.com>
2023-03-29 07:11:20 +0700

    Initial Commit

---
+++ Fructus/FructusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FructusApp.swift
+//  Fructus
+//
+//  Created by Rifqi Muhammad Aziz on 29/03/23.
+//
+
+import SwiftUI
+
+@main
+struct FructusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
