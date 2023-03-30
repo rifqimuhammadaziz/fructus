ae041e09cd426c95ca511378c14ecbd154563fc6
rifqimuhammadaziz <rifqimuhammadaziz@gmail.com>
2023-03-30 10:01:29 +0700

    fetching data to onboarding view

--- Fructus/App/OnboardingView.swift
+++ Fructus/App/OnboardingView.swift
@@ -10,12 +10,13 @@ import SwiftUI
 struct OnboardingView: View {
     // MARK: - PROPERTIES
     
+    var fruits: [Fruit] = fruitsData
+    
     // MARK: - BODY
     var body: some View {
         TabView {
-            ForEach(0..<5) { item in
-//                FruitCardView()
-                Text("Cards")
+            ForEach(fruits[0...5]) { fruit in
+                FruitCardView(fruit: fruit)
             } //: LOOP
         } //: TAB
         .tabViewStyle(PageTabViewStyle())
@@ -27,6 +28,6 @@ struct OnboardingView: View {
 
 struct OnboardingView_Previews: PreviewProvider {
     static var previews: some View {
-        OnboardingView()
+        OnboardingView(fruits: fruitsData)
     }
 }
